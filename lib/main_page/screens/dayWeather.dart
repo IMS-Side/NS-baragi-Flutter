c07def739cafda17fa4e3bede1e8b9a318319b5c
나래 <skgus6699@naver.com>
2025-01-17 11:21:16 +0900

    [mainpage] ui : dayWeather.dart, otherWeatherCard.dart 클래스 위젯 기본 구조 생성

---
+++ lib/main_page/screens/dayWeather.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class  DayWeather extends StatefulWidget {
+  const DayWeather({super.key});
+
+  @override
+  State<DayWeather> createState() => _DayWeatherState();
+}
+
+class _DayWeatherState extends State<DayWeather> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
---
+++ lib/main_page/widgets/otherWeatherCard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class OtherWeatherCard extends StatefulWidget {
+  const OtherWeatherCard({super.key});
+
+  @override
+  State<OtherWeatherCard> createState() => _OtherWeatherCardState();
+}
+
+class _OtherWeatherCardState extends State<OtherWeatherCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Row(
+        
+      ),
+    );
+  }
+}
