0b71cb3ecf503d5c48dde84fe3a31f20099f7b62
나래 <skgus6699@naver.com>
2025-01-17 11:20:07 +0900

    [mainpage] ui : tapbar 항목(체감날씨, 옷추천 등) 추가.

---
+++ lib/main_page/tapbar/tapbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class tapbar extends StatefulWidget {
+  const tapbar({super.key});
+
+  @override
+  State<tapbar> createState() => _tapbarState();
+}
+
+class _tapbarState extends State<tapbar> with SingleTickerProviderStateMixin {
+
+  late TabController tabController = TabController(
+    length: 3, vsync: this, initialIndex: 0,
+
+    // 탭 변경 애니메이션 시간
+    animationDuration: const Duration(milliseconds: 800),
+  );
+
+  void dispose(){
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TapBarScreen"),
+      ),
+      body: _tapBar(), //tapbar 적용
+    );
+  }
+
+  //만약 속성 변경(스타일 변경을 하고 싶다면) 여기에서
+  Widget _tapBar(){
+    return TabBar(
+      controller: tabController,
+      labelColor: Colors.black, //나중에 white로 바꿔야함.
+      labelStyle: const TextStyle(
+        fontSize: 12,
+        //추가 폰트 작업은 추후 여기에서 
+      ),
+      tabs: const [
+        Tab(text: "날씨"),
+        Tab(text: "체감 날씨"),
+        Tab(text: "옷 추천"),
+      ],
+    );
+  }
+
+
+}
