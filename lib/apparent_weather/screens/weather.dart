692ee8fb9fe9b84a30d6df0bf9eb066de015384c
niireymik <155354743+niireymik@users.noreply.github.com>
2025-01-22 01:33:31 +0900

    [apparent-weather-page] ui : 현재 날씨 정보를 나타내는 weather 위젯 구현

---
+++ lib/apparent_weather/screens/weather.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatelessWidget {
+  const Weather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          '정왕동',
+          style: TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 13, height: 1.7, color: Colors.white),
+        ),
+        SizedBox(height: 9),
+        Container(
+          width: 392,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 106,
+                height: 84,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        '현재 기온',
+                        style: TextStyle(fontFamily: 'PretendardRegular',fontSize: 12, height: 1.0, color: Colors.white)
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                        '88º',
+                        style: TextStyle(fontFamily: 'PretendardRegular', fontSize: 64, height: 1.0, color: Colors.white)
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(width: 60),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        '체감 기온',
+                        style: TextStyle(fontFamily: 'PretendardRegular', fontSize: 12, height: 1.0, color: Colors.white)
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                        '03º',
+                        style: TextStyle(fontFamily: 'PretendardRegular', fontSize: 64, height: 1.0, color: Colors.white)
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 26),
+        Text(
+            '맑음',
+            style: TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 12, height: 1.7, color: Colors.white)
+        ),
+        SizedBox(height: 1),
+        Text(
+            '최고: 13º 최저: -88º',
+            style: TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 12, height: 1.7, color: Colors.white)
+        ),
+        SizedBox(height: 2),
+      ],
+    );
+  }
+}
