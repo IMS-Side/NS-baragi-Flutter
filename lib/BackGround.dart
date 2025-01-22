70a51e99d92b8eed1c5ec7a64faf17eb608da308
나래 <skgus6699@naver.com>
2025-01-22 19:01:48 +0900

    [feature/mainpage] feat : 조건문 사용을 위한 색상 데이터 타입 추가

--- lib/BackGround.dart
+++ lib/BackGround.dart
@@ -3,54 +3,41 @@ import 'package:flutter/cupertino.dart';
 class Background extends StatelessWidget {
   final Widget child;
 
-  const Background({required this.child, super.key});
+  Background({required this.child, super.key});
 
   //여기다가 이제 시간대별로 어떻게 보일지 조정해서 색을 추가하면 됨.
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color(0xFF4EB5F4),
-            Color(0xFFA2CFF9),
-            Color(0x80D2DAEC),
-          ],
-          stops: [
-            0.13, // 13% 위치
-            0.70, // 70% 위치
-            1.0,  // 100% 위치
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: child,
-    );
-  }
-}
+  final basic = [
+    const Color(0xFF4EB5F4),
+    const Color(0xFFA2CFF9),
+    const Color(0x80D2DAEC),
+  ];
+  final basic_stop = [
+    0.13, // 13% 위치
+    0.70, // 70% 위치
+    1.0,  // 100% 위치
+  ];
+  final suset = [
+    const Color(0xFF342D95),
+    const Color(0xFFA480CF),
+    const Color(0x80E99783),
+  ];
+  final suset_stop = [
+    0.13,
+    0.56,
+    0.84,
+  ];
 
-class SunSetBackground extends StatelessWidget {
-
-  final Widget child;
-  const SunSetBackground({super.key, required this.child});
+  //final background;
+  // 조건문으로 시간에 따라서 background 변수에
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color(0xFF342D95),
-            Color(0xFFA480CF),
-            Color(0x80E99783),
-          ],
-          stops: [
-            0.13, // 13% 위치
-            0.56, // 70% 위치
-            0.84,  // 100% 위치
-          ],
+          colors: basic,
+          stops: basic_stop,
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
