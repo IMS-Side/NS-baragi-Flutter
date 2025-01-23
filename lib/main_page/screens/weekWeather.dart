ebd7a5e5ee72d053a7c32575c24b5f20246f1268
나래 <skgus6699@naver.com>
2025-01-24 01:44:07 +0900

    [feature/mainpage] ui : weekWeather.dart 구분선 추가

--- lib/main_page/screens/weekWeather.dart
+++ lib/main_page/screens/weekWeather.dart
@@ -13,29 +13,32 @@ class WeekWeather extends StatefulWidget {
 class _WeekWeatherState extends State<WeekWeather> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 392,
-      margin: const EdgeInsets.all(2),
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-          color: const Color(0x33467ABE),
-          borderRadius: BorderRadius.circular(15)
-      ),
-      child: ListView.separated(
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 7,
-          itemBuilder: (context, index){
-              return const Padding(
-                padding: EdgeInsets.only(bottom: 2.0),
-                child: WeekCard(),
-              );
-          },
-          separatorBuilder: (context, index){
-            return const Divider(
-              color: Color(0xFF8FC5E9),
-            );
-            },
+    return Center(
+      child: Container(
+        width: 392,
+        margin: const EdgeInsets.all(2),
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+            color: const Color(0x33467ABE),
+            borderRadius: BorderRadius.circular(15)
         ),
+        child: ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+            itemCount: 7,
+            itemBuilder: (context, index){
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 2.0),
+                  child: WeekCard(),
+                );
+            },
+            separatorBuilder: (context, index){
+              return const Divider(
+                color: Color(0xFF8FC5E9),
+              );
+              },
+          ),
+      ),
     );
   }
 }
