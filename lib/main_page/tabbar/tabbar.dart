1a02df7d14d96d493abe766b1a996e4dc36c0c9a
나래 <skgus6699@naver.com>
2025-01-28 03:16:51 +0900

    [feature/mainpage] ui : tabBar 위치 수정에 따른 구조 변경

--- lib/main_page/tabbar/tabbar.dart
+++ lib/main_page/tabbar/tabbar.dart
@@ -7,30 +7,33 @@ class TabBarScreen extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 240,
-      height: 30,
-      //alignment: ,
-      color: Color(0xFF4EB5F4), //나중에 색깔 변경
-      child: TabBar(
-        controller: tabController,
-        labelColor: Colors.white, // 탭 선택 시의 글자 색상
-        unselectedLabelColor: Colors.white, // 선택되지 않은 탭 텍스트 색상
-        indicatorColor: Colors.white, // 선택된 탭 아래 표시자 색상
-        //indicator: const BoxDecoration(),
-        indicatorSize: TabBarIndicatorSize.tab,
-        labelStyle: const TextStyle(
-          color: Colors.white,
-          fontSize: 12,
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        width: 240,
+        height: 30,
+        //alignment: ,
+        color: Color(0xFF4EB5F4), //나중에 색깔 변경
+        child: TabBar(
+          controller: tabController,
+          labelColor: Colors.white, // 탭 선택 시의 글자 색상
+          unselectedLabelColor: Colors.white, // 선택되지 않은 탭 텍스트 색상
+          indicatorColor: Colors.white, // 선택된 탭 아래 표시자 색상
+          //indicator: const BoxDecoration(),
+          indicatorSize: TabBarIndicatorSize.tab,
+          labelStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontFamily: 'PretendardSemiBold',
+          ),
+          tabs: const [
+            Tab(child: Text("날씨")),
+            Tab(child: Text("체감 날씨")),
+            Tab(child: Text("옷 추천")),
+            //Tab(child: Icon(Icons.location_on),)
+          ],
         ),
-        tabs: const [
-          Tab(child: Text("날씨")),
-          Tab(child: Text("체감 날씨")),
-          Tab(child: Text("옷 추천")),
-          //Tab(child: Icon(Icons.location_on),)
-        ],
       ),
-
     );
   }
 
