9e903eaaabf4d35d2f2900ca39f35b0a053d09ee
나래 <skgus6699@naver.com>
2025-03-19 17:14:47 +0900

    [feature/mainpage] feat : 날씨 및 시간 별로 배경색 변경 기능 추가

--- lib/BackGround.dart
+++ lib/BackGround.dart
@@ -1,10 +1,15 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:nsbaragi/main_page/controllers/shortWeatherController.dart';
 
 class Background extends StatelessWidget {
   final Widget child;
 
   Background({required this.child, super.key});
 
+  final ShortWeatherController shortWeatherController = Get.find<ShortWeatherController>();
   //여기다가 이제 시간대별로 어떻게 보일지 조정해서 색을 추가하면 됨.
 
   final basic = [
@@ -63,19 +68,66 @@ class Background extends StatelessWidget {
   //final background;
   // 조건문으로 시간에 따라서 background 변수에
 
+  List<Color> getBackgroundColors() {
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    //날씨 상태에 따라 배경 변경
+    final description = shortWeatherController.weatherDescription; //하늘 상태 가져오기
+    if(description.contains('눈')) return snow;
+    if(description.contains('비') || description.contains('흐림')) return cloudy;
+
+    log("$now , $hour");
+
+    //시간대
+    if(hour >= 8 && hour <= 18){
+      return basic;
+    }else if(hour > 18 && hour <= 19){
+      return sunset;
+    }else{
+      return night;
+    }
+  }
+
+  // 그라디언트 스톱 반환
+  List<double> getGradientStops() {
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    //날씨 상태에 따라 배경 변경
+    final description = shortWeatherController.weatherDescription; //하늘 상태 가져오기
+    if(description.contains('눈')) return snow_stop;
+    if(description.contains('구름') || description.contains('흐림')) return cloudy_stop;
+
+
+    if (hour >= 6 && hour < 12) {
+      return basic_stop; // 아침
+    } else if (hour >= 12 && hour < 18) {
+      return sunset_stop; // 석양
+    } else {
+      return night_stop; // 밤
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: basic,
-          stops: basic_stop,
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+    return Obx((){
+      final colors = getBackgroundColors();
+      final stops = getGradientStops();
+
+      return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            stops: stops,
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
-      ),
-      child: child,
-    );
+        child: child,
+      );
+    });
   }
 }
 
